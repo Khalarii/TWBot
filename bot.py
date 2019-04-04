3a5ffd38aa72ff2a1feeb34806a857f9ee45c631
Khalarii <kimgss94@hotmail.com>
2019-04-04 19:12:27 -0300

    Removed 50 village check

--- bot.py
+++ bot.py
@@ -183,34 +183,26 @@ class Bot:
 		return attack_units > 0 and attack_units <= self.get_available_units(unit_type)
 
 	def farm_villages(self, player_village, unit_type):
-		count = 0
 		self.current_village = player_village
 		first_village_run_at = datetime.datetime.now()
 
 		for village in self.current_village.villages_to_farm:
-			count += 1
+			self.stop_if_captcha()
+			sleep(self.u.get_random_float(1,2))
+			current_url = self.browser.current_url
 
-			if count <= 50:
-				self.stop_if_captcha()
-				sleep(self.u.get_random_float(1,2))
-				current_url = self.browser.current_url
+			if current_url != self.meeting_place_URL.format(self.world.id, self.current_village.village_id) and current_url != self.secondary_meeting_place_URL.format(self.world.id, self.current_village.village_id):
+				self.go_to_meeting_place()
 
-				if current_url != self.meeting_place_URL.format(self.world.id, self.current_village.village_id) and current_url != self.secondary_meeting_place_URL.format(self.world.id, self.current_village.village_id):
-					self.go_to_meeting_place()
-
-				if (datetime.datetime.now() - first_village_run_at) > datetime.timedelta(minutes=40):
-					self.u.print_with_time_stamp("40 minute timeout")
-					self.u.print_with_time_stamp("Going to next village")
-					return
-					#self.farm_villages(unit_type)
-				else if enough_units(village, unit_type):
-					self.send_attack(village, unit_type)
-				else:
-					self.u.print_with_time_stamp("Not enough units to attack")
-					self.u.print_with_time_stamp("Going to next village")
-					return
+			if (datetime.datetime.now() - first_village_run_at) > datetime.timedelta(minutes=40):
+				self.u.print_with_time_stamp("40 minute timeout")
+				self.u.print_with_time_stamp("Going to next village")
+				return
+				#self.farm_villages(unit_type)
+			else if enough_units(village, unit_type):
+				self.send_attack(village, unit_type)
 			else:
-				self.u.print_with_time_stamp("50 villages farmed")
+				self.u.print_with_time_stamp("Not enough units to attack")
 				self.u.print_with_time_stamp("Going to next village")
 				return
 
